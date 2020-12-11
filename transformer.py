85de167aed2d50d59d3e8431e3bb21185e74d200
bshantam97 <bshantam97@gmail.com>
2020-12-12 01:46:28 +0530

    Added encoder-decoder method stubs

--- transformer.py
+++ transformer.py
@@ -75,3 +75,21 @@ class TransformerBlock(nn.Module):
         forward = self.feed_forward(x)
         out = self.dropout(self.norm2(forward + x))
         return out
+
+class Encoder(nn.Module):
+
+    def __init__(self):
+
+        super(Encoder, self).__init__()
+    
+    def forward(self):
+        pass
+
+class Decoder(nn.Module):
+
+    def __init__(self):
+
+        super(Decoder, self).__init__()
+    
+    def forward(self):
+        pass
